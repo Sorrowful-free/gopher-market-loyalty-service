140347760b178c0a16a7802e7441f884cfc9b383
Sergei Kotkin <sorrowful.free@gmail.com>
2025-11-03 10:59:27 +0300

    - fix first migration

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -1,2 +1,5 @@
-DROP INDEX IF EXISTS idx_user_id;
-DROP TABLE IF EXIST users;
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    login VARCHAR(64) pass_hash VARCHAR(255)
+);
+CREATE INDEX idx_user_id ON users(id);
